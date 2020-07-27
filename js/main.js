96bf8eac62b8776d8e888989e9b9968ab0036a8e
Mahmoud Elshamy <shamyyoun@gmail.com>
2020-07-27 20:47:53 +0200

    Implemnted slider dots

--- js/main.js
+++ js/main.js
@@ -1,11 +1,21 @@
 
 $(document).ready(function () {
 
+  // Mixit up
   $('#Container').mixItUp();
   new WOW().init();
 
+  // Collapse
   $('.collapse').collapse();
 
+  // Init flipster carousel
+  const carousel = $("#carousel").flipster({
+    style: 'carousel',
+    spacing: -0.5,
+    nav: true,
+    buttons: false,
+    keyboard: false
+  });
 
    // draw slider dots
   const $sliderItems = $('#carousel .flipster__item');
@@ -16,10 +26,30 @@ $(document).ready(function () {
 
     $sliderDots.append(
       `<li>
-        <a class="item slider-dot ${dotClasses}"><span></span></a>
+        <a class="item slider-dot ${dotClasses}" data-index=${i}><span></span></a>
       </li>`
     );
   }
+
+  // add dots click listeners
+  $sliderDots.on('click', '.slider-dot', function(e) {
+    // remove dots
+    $('.slider-dot').removeClass('active');
+    
+    const $dot = $(e.target);
+    $dot.addClass('active');
+    const index = parseInt($dot.data('index'));
+    const $flipsterNavItem = $(`.flipster__nav__link:eq(${index})`);
+    console.log($flipsterNavItem);
+    $flipsterNavItem.click();
+  });
+
+  // adding click listener to the filpster items to sync with dots
+  $('li.flipster__item').on('click', function() {
+    const $item = $(this);
+    $('.slider-dot').removeClass('active');
+    $(`.slider-dot[data-index=${$item.index()}]`).addClass('active');
+  });
 
 });
 
@@ -30,13 +60,3 @@ $(function () {
     $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
   });
 });
-var carousel = $("#carousel").flipster({
-  style: 'carousel',
-  spacing: -0.5,
-  nav: true,
-  buttons: true,
-});
-
-
-
-
